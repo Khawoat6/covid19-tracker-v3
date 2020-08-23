5b54de05145fd25ccf11674439a01964dfbbc799
oat phattaraphon <phattaraphon.c@ku.th>
2020-08-23 15:28:17 +0700

    get api countries on dropdown

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { MenuItem, FormControl, Select } from '@material-ui/core';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  // STATE = HOW to write a variable in REACT <<<<<<
+
+  // https://disease.sh/v3/covid-19/countries
+
+  // USE EFFECT = Runs a piece of code
+  // based on a given condition
+
+  useEffect(() => {
+    // The code inside here will run once
+    // when the component loads and not again
+    // async -> send a request, wait for it, do something
+
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country, // United States, United Kingdom
+            value: country.countryInfo.iso2, // UK, USE, FR
+          }));
+
+          setCountries(countries);
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Lets build a COVID 19 tracker Clever Programmers!!</h1>
+      <div className="app__header">
+        <h1>COVID-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {/* Loop through all the countries and  
+            show a dropdown list of the options */}
+
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+
+            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Option two</MenuItem>
+            <MenuItem value="worldwide">Option tree</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
 
       {/* Header */}
       {/* Title + Select input dropdown field */}
