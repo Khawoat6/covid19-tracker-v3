4a353cc418ba657f2597182558fe849fc940ffda
oat phattaraphon <phattaraphon.c@ku.th>
2020-08-23 14:22:10 +0700

    first commit for connect direc-repo

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lets build a COVID 19 tracker Clever Programmers!!</h1>
+
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
+    </div>
+  );
+}
+
+export default App;
